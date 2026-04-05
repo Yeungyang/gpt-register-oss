b52b6a3c5166f908712f389ca1dc3a8d837a82c0
root <root@ser309693356983.local>
2026-04-05 18:38:19 +0000

    提交说明

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import preact from "@preact/preset-vite";
 export default defineConfig({
   plugins: [preact()],
   server: {
-    host: "127.0.0.1",
+    host: "0.0.0.0",
     port: 8173,
     proxy: {
       "/api": {
